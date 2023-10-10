7e583d48a8efcf7173176ee85ed133c0958a9df8
ArthurM <arthurmonteiro20172018@gmail.com>
2023-10-09 22:20:55 -0300

    Initial commit

---
+++ ApiGrupo5/src/main/java/br/com/api/aula/ApiGrupo5Application.java
@@ -0,0 +1,13 @@
+package br.com.api.aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGrupo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGrupo5Application.class, args);
+	}
+
+}
