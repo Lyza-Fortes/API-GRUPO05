8bbf02b8739808e18cd896f72272b9db587d20a5
Taissa <taissabastosgouvea@gmail.com>
2023-10-12 15:50:21 -0300

    feat: CRUD simples em CategoriaService, identificação dos repositótios.

--- ApiGrupo5/src/main/java/br/com/api/g5/repositories/FuncionarioRepository.java
+++ ApiGrupo5/src/main/java/br/com/api/g5/repositories/FuncionarioRepository.java
@@ -1,5 +1,11 @@
 package br.com.api.g5.repositories;
 
-public class FuncionarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.api.g5.entities.Funcionario;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer>{
 
 }
