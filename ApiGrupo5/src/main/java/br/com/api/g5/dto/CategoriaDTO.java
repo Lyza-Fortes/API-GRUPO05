ab2e6aa4e0247862662731508dbc4aa6d7105903
VictorSoares-C <victorsoares.c@gmail.com>
2023-10-22 18:21:11 -0300

    feat: Criação das classes DTOs

---
+++ ApiGrupo5/src/main/java/br/com/api/g5/dto/CategoriaDTO.java
@@ -0,0 +1,35 @@
+package br.com.api.g5.dto;
+
+public class CategoriaDTO {
+	
+	private String nome;
+	private String descricao;
+	
+	public CategoriaDTO(String nome, String descricao) {
+		super();
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	public CategoriaDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
